985e6a39217c7d0346dcf2077a0cdace694bb406
henkeklein <henrikklein@hotmail.com>
2016-02-24 10:05:18 +0100

    push2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.print("haha");
         System.out.print("labb");
-        System.out.print("HEEEEEJ");
+        System.out.print("HEEEJ");
 
     }
 }
